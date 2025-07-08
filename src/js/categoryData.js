712665852f42905ccdcf38274ccdf5c85539e337
CODECLOUDS-DIBAKAR <codeclouds-dibakar@CISPL-IO-610.local>
2025-07-08 20:16:13 +0530

    Fix Image Path

--- src/js/categoryData.js
+++ src/js/categoryData.js
@@ -3,25 +3,25 @@ const categoryData = [
     id: 1,
     title: "Earrings",
     productCount: 4,
-    imageUrl: "src/assets/images/earrings.jpg",
+    imageUrl: "/src/assets/images/earrings.jpg",
   },
   {
     id: 2,
     title: "Necklaces",
     productCount: 3,
-    imageUrl: "src/assets/images/necklaces.jpg",
+    imageUrl: "/src/assets/images/necklaces.jpg",
   },
   {
     id: 3,
     title: "Rings",
     productCount: 5,
-    imageUrl: "src/assets/images/rings.jpg",
+    imageUrl: "/src/assets/images/rings.jpg",
   },
   {
     id: 4,
     title: "Bracelets",
     productCount: 3,
-    imageUrl: "src/assets/images/bracelets.jpg",
+    imageUrl: "/src/assets/images/bracelets.jpg",
   },
 ];
 
